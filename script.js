d9c392a72fbbe5647e949af937883f6a41a3af2e
Denna Bellman <dennabellman@gmail.com>
2019-10-31 14:00:46 -0400

    fixed css

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ fetch("prints.json")
     .then(res => res.json())
     .then(data => {
         // console.log(data[0].name);
-        for (let product of data)
+
+        let section = document.querySelector('.products');
+        for (let product of data) {
+            let div = document.createElement("div");
+            div.innerText = product.name;
+            div.classList.add('div');
+            section.appendChild(div);
+        }
     })
 
-// let bodyEl = document.querySelector("body");
 
-for (let i = 0; i <= 10; i++) {
-    let div = document.createElement("div");
-    bodyEl.appendChild(div);
-}
