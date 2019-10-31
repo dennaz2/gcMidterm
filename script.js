5a74214ebdc43a76776618a4fcdc84a2e57ca774
JD <jdandrews357@gmail.com>
2019-10-31 11:25:36 -0400

    for loop

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 "use strict";
+
+const newArray = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+
+let bodyEl = document.querySelector("body");
+
+for (let i = 0; i <= 10; i++) {
+  let div = document.createElement("div");
+  bodyEl.appendChild(div);
+}
