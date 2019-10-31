591afd7468e6de13c17ef505c165f9f70a9276f3
JD <jdandrews357@gmail.com>
2019-10-31 14:20:11 -0400

    json update

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 "use strict";
 
 fetch("prints.json")
-    .then(res => res.json())
-    .then(data => {
-        // console.log(data[0].name);
+  .then(res => res.json())
+  .then(data => {
+    // console.log(data[0].name);
 
-        let section = document.querySelector('.products');
-        for (let product of data) {
-            let div = document.createElement("div");
-            div.innerText = product.name;
-            div.classList.add('div');
-            section.appendChild(div);
-        }
-    })
-
-
+    let section = document.querySelector(".products");
+    for (let product of data) {
+      let div = document.createElement("div");
+      div.innerText = product.name;
+      div.classList.add("div");
+      section.appendChild(div);
+    }
+  });
