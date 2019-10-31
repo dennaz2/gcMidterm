e4ab8c14d4389182bec8b5250ae4eb0ccd2e26c5
kenneyaisho <kenneyaisho@gmail.com>
2019-10-31 13:43:50 -0400

    changed font and corrected grammar in header

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ const newArray = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
 let bodyEl = document.querySelector("body");
 
 for (let i = 0; i <= 10; i++) {
-  let div = document.createElement("div");
-  bodyEl.appendChild(div);
+    let div = document.createElement("div");
+    bodyEl.appendChild(div);
 }
