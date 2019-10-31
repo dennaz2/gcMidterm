f702c9ddb0c700df7a9bc573aa72414506f2d5fe
Denna Bellman <dennabellman@gmail.com>
2019-10-31 13:46:21 -0400

    updated script

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 "use strict";
 
-const newArray = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+fetch("prints.json")
+    .then(res => res.json())
+    .then(data => {
+        // console.log(data[0].name);
+        for (let product of data)
+    })
 
-let bodyEl = document.querySelector("body");
+// let bodyEl = document.querySelector("body");
 
 for (let i = 0; i <= 10; i++) {
     let div = document.createElement("div");
