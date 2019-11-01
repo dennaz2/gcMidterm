5a11820d31de4039faef5b8f6459345dbc78c423
Denna Bellman <dennabellman@gmail.com>
2019-11-01 10:27:10 -0400

    updates

--- script.js
+++ script.js
@@ -2,62 +2,57 @@
 
 let items = null; //used to get the data from inside of the fetch
 fetch("prints.json")
-  .then(res => res.json())
-  .then(data => {
-    // console.log(data[0].name);
-    items = data;
-    let section = document.querySelector(".products");
-    for (let product of data) {
-      //divs
-      let div = document.createElement("div");
-      div.innerText = `${product.name}`;
-      div.classList.add("div");
-      section.appendChild(div);
+    .then(res => res.json())
+    .then(data => {
+        // console.log(data[0].name);
+        items = data;
+        let section = document.querySelector(".products");
+        data.forEach((product, index) => { 
+            //divs
+            let div = document.createElement("div");
+            div.innerText = `${product.name} ${product.description}`;
+            div.classList.add("div");
+            section.appendChild(div);
 
-      //images
-      let image = document.createElement("img");
-      image.classList.add("img");
-      image.setAttribute("src", product.image);
-      div.appendChild(image);
+            //images
+            let image = document.createElement("img");
+            image.classList.add("img");
+            image.setAttribute("src", product.image);
+            div.appendChild(image);
 
-      //description
+            //price
+            let menu = document.createElement("select");
 
-      let descriptionEl = document.createElement("div");
-      descriptionEl.innerText = `${product.description}`;
-      descriptionEl.classList.add("description");
-      div.appendChild(descriptionEl);
+            //options
+            for (const option of product.price) {
+                let options = document.createElement("option");
+                options.innerText = option;
+                menu.appendChild(options);
+                div.appendChild(menu);
+            }
 
-      //price
-      let menu = document.createElement("select");
-
-      //options
-      for (const option of product.price) {
-        let options = document.createElement("option");
-        options.innerText = option;
-        menu.appendChild(options);
-        div.appendChild(menu);
-      }
-
-      //button for adding items to cart.
-      let addItem = document.createElement("button");
-      addItem.id = "btn";
-      addItem.setAttribute("type", "submit");
-      addItem.innerText = "Add to cart";
-      div.appendChild(addItem);
-    }
-  });
+            //button for adding items to cart.
+            let addItem = document.createElement("button");
+            addItem.id = "btn";
+            addItem.setAttribute("type", "submit");
+            addItem.innerText = "Add to cart";
+            div.appendChild(addItem);
+        }
+    )});
 
 let cart = [];
 
-//add items button
-let container = document.querySelector(".products");
-container.addEventListener("click", addToCart);
+//add items button 
+// let container = document.querySelector(".products")
+// container.addEventListener('click', addToCart);
 
-function addToCart(event) {
-  if (event.target.id === "btn") {
-    console.log("This works");
-  }
-}
+// function addToCart(event) {
+//     if (event.target.id === "btn") {
+
+//         cart.push(items[]);
+//         console.log(cart);
+//     }
+// }
 
 // document.querySelector('button').addEventListener('click', function () {
 //     console.log(items);
