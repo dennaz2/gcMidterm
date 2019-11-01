15600239d5701241af4be7b089aff4c1d6c2f162
JD <jdandrews357@gmail.com>
2019-11-01 11:10:38 -0400

    price fix and others

--- script.js
+++ script.js
@@ -3,69 +3,74 @@
 let items = null; //used to get the data from inside of the fetch
 let div;
 fetch("prints.json")
-    .then(res => res.json())
-    .then(data => {
-        // console.log(data[0].name);
-        items = data;
-        let section = document.querySelector(".products");
-        data.forEach((product, index) => {
-            //divs
+  .then(res => res.json())
+  .then(data => {
+    // console.log(data[0].name);
+    items = data;
+    let section = document.querySelector(".products");
+    data.forEach((product, index) => {
+      //divs
+      let div = document.createElement("div");
+      div.classList.add("div");
+      section.appendChild(div);
 
-            let div = document.createElement("div");
-            div.setAttribute('data-index-number', index);
-            div.innerText = `${product.name}`;
-            div.classList.add("div");
-            section.appendChild(div);
+      //title
+      let titleEl = document.createElement("div");
+      titleEl.innerText = `${product.name}`;
+      titleEl.classList.add("title");
+      div.appendChild(titleEl);
 
+      //images
+      let image = document.createElement("img");
+      image.classList.add("img");
+      image.setAttribute("src", product.image);
+      div.appendChild(image);
 
-            //images
-            let image = document.createElement("img");
-            image.classList.add("img");
-            image.setAttribute("src", product.image);
-            div.appendChild(image);
+      //description
+      let descriptionEl = document.createElement("div");
+      descriptionEl.innerText = `${product.description}`;
+      descriptionEl.classList.add("description");
+      div.appendChild(descriptionEl);
 
-            //description
-            let descriptionEl = document.createElement("div");
-            descriptionEl.innerText = `${product.description}`;
-            descriptionEl.classList.add("description");
-            div.appendChild(descriptionEl);
+      //   //price
+      //   let menu = document.createElement("select");
 
-            //price
+      //   //options
+      //   for (const option of product.price) {
+      //     let options = document.createElement("option");
+      //     options.innerText = option;
+      //     menu.appendChild(options);
+      //     div.appendChild(menu);
+      //   }
 
-        
-            // let menu = document.createElement("select");
+      //priceAss
+      let priceEl = document.createElement("div");
+      priceEl.innerText = `$${product.priceAss}`;
+      div.appendChild(priceEl);
 
-            // //options
-            // for (const option of product.price) {
-            //     let options = document.createElement("option");
-            //     options.innerText = option;
-            //     menu.appendChild(options);
-            //     div.appendChild(menu);
-            // }
-
-            //button for adding items to cart.
-            let addItem = document.createElement("button");
-            addItem.id = "btn";
-            addItem.setAttribute("type", "submit");
-            addItem.innerText = "Add to cart";
-            div.appendChild(addItem);
-        });
+      //button for adding items to cart.
+      let addItem = document.createElement("button");
+      addItem.id = "btn";
+      addItem.setAttribute("type", "submit");
+      addItem.innerText = "Add to cart";
+      div.appendChild(addItem);
     });
+  });
 
 let cart = [];
 
 // add items button
-let container = document.querySelector(".products")
-container.addEventListener('click', addToCart);
+let container = document.querySelector(".products");
+container.addEventListener("click", addToCart);
 
 function addToCart(event) {
-    if (event.target.id === "btn") {
-        console.log(event.target);
-        console.log(event.target.parentElement);
-        const index = event.target.parentElement.getAttribute('data-index-number');
-        cart.push(items[index]);
-        console.log(cart);
-    }
+  if (event.target.id === "btn") {
+    console.log(event.target);
+    console.log(event.target.parentElement);
+    const index = event.target.parentElement.getAttribute("data-index-number");
+    cart.push(items[index]);
+    console.log(cart);
+  }
 }
 
 // document.querySelector('button').addEventListener('click', function () {
