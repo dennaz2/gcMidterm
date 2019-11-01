f2724b053576bc34235df6d7d2592dd3f7e5c41e
Denna Bellman <dennabellman@gmail.com>
2019-11-01 10:55:42 -0400

    updates

--- script.js
+++ script.js
@@ -1,64 +1,72 @@
 "use strict";
 
 let items = null; //used to get the data from inside of the fetch
+let div;
 fetch("prints.json")
-  .then(res => res.json())
-  .then(data => {
-    // console.log(data[0].name);
-    items = data;
-    let section = document.querySelector(".products");
-    data.forEach((product, index) => {
-      //divs
-      let div = document.createElement("div");
-      div.innerText = `${product.name} ${product.description}`;
-      div.classList.add("div");
-      section.appendChild(div);
+    .then(res => res.json())
+    .then(data => {
+        // console.log(data[0].name);
+        items = data;
+        let section = document.querySelector(".products");
+        data.forEach((product, index) => {
+            //divs
 
-      //images
-      let image = document.createElement("img");
-      image.classList.add("img");
-      image.setAttribute("src", product.image);
-      div.appendChild(image);
+            let div = document.createElement("div");
+            div.setAttribute('data-index-number', index);
+            div.innerText = `${product.name}`;
+            div.classList.add("div");
+            section.appendChild(div);
 
-      //description
-      let descriptionEl = document.createElement("div");
-      descriptionEl.innerText = `${product.description}`;
-      descriptionEl.classList.add("description");
-      div.appendChild(descriptionEl);
 
-      //price
-      let menu = document.createElement("select");
+            //images
+            let image = document.createElement("img");
+            image.classList.add("img");
+            image.setAttribute("src", product.image);
+            div.appendChild(image);
 
-      //options
-      for (const option of product.price) {
-        let options = document.createElement("option");
-        options.innerText = option;
-        menu.appendChild(options);
-        div.appendChild(menu);
-      }
+            //description
+            let descriptionEl = document.createElement("div");
+            descriptionEl.innerText = `${product.description}`;
+            descriptionEl.classList.add("description");
+            div.appendChild(descriptionEl);
 
-      //button for adding items to cart.
-      let addItem = document.createElement("button");
-      addItem.id = "btn";
-      addItem.setAttribute("type", "submit");
-      addItem.innerText = "Add to cart";
-      div.appendChild(addItem);
+            //price
+
+        
+            // let menu = document.createElement("select");
+
+            // //options
+            // for (const option of product.price) {
+            //     let options = document.createElement("option");
+            //     options.innerText = option;
+            //     menu.appendChild(options);
+            //     div.appendChild(menu);
+            // }
+
+            //button for adding items to cart.
+            let addItem = document.createElement("button");
+            addItem.id = "btn";
+            addItem.setAttribute("type", "submit");
+            addItem.innerText = "Add to cart";
+            div.appendChild(addItem);
+        });
     });
-  });
 
 let cart = [];
 
-//add items button
-// let container = document.querySelector(".products")
-// container.addEventListener('click', addToCart);
+// add items button
+let container = document.querySelector(".products")
+container.addEventListener('click', addToCart);
 
-// function addToCart(event) {
-//     if (event.target.id === "btn") {
-
-//         cart.push(items[]);
-//         console.log(cart);
-//     }
-// }
+function addToCart(event) {
+    if (event.target.id === "btn") {
+        console.log(event.target);
+        console.log(event.target.parentElement);
+        const index = event.target.parentElement.getAttribute('data-index-number');
+        cart.push(items[index]);
+        console.log(cart);
+    }
+}
 
 // document.querySelector('button').addEventListener('click', function () {
 //     console.log(items);
